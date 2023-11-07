e1b6fc3e4776f7f1c4770da45898007bdc6c363f
Atacan <Atacan@DESKTOP-9N004FT>
2023-11-07 22:13:04 +0300

    implementation of Task2

--- Exception Handling/Task2/NumberParser.cs
+++ Exception Handling/Task2/NumberParser.cs
@@ -6,7 +6,58 @@ namespace Task2
     {
         public int Parse(string stringValue)
         {
-            throw new NotImplementedException();
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException("Value cannot be null or empty.");
+            }
+
+            if (stringValue == string.Empty)
+            {
+                throw new FormatException("Value contains non-numeric characters.");
+            }
+
+            if (!string.IsNullOrWhiteSpace(stringValue))
+            {
+                stringValue = stringValue.Trim();
+            }
+
+            int result = 0;
+            bool isNegative = false;
+            int startIndex = 0;
+
+            if (stringValue[0] == '-')
+            {
+                isNegative = true;
+                startIndex = 1;
+            }
+            else if (stringValue[0] == '+')
+            {
+                startIndex = 1;
+            }
+
+            for (int i = startIndex; i < stringValue.Length; i++)
+            {
+                char currentChar = stringValue[i];
+
+                if (currentChar < '0' || currentChar > '9')
+                {
+                    throw new FormatException("Value contains non-numeric characters.");
+                }
+
+                int currentDigit = currentChar - '0';
+
+                if (!isNegative)
+                {
+                    result = checked(result * 10 + currentDigit);
+                }
+                else
+                {
+                    result = checked(result * 10 - currentDigit);
+                }
+
+            }
+
+            return result;
         }
     }
 }
