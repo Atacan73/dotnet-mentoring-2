dae435f2a74c9aa003ddb8772df1149f7800c3a1
Atacan <Atacan@DESKTOP-9N004FT>
2023-11-07 22:13:04 +0300

    implementation of Task3

--- Exception Handling/Task1/Program.cs
+++ Exception Handling/Task1/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            Console.WriteLine("Please enter a string:");
+            string input = Console.ReadLine();
+
+            GetFirstChar(input);
+        }
+
+        private static void GetFirstChar(string input)
+        {
+            try
+            {
+                Console.WriteLine("First char: " + input[0]);
+            }
+            catch (Exception ex)
+            {
+                // Might be the Index out of range exception unnecceseary? because always returns first element and if it is null, then array is empty.
+                if (ex is IndexOutOfRangeException || ex is ArgumentNullException || ex is ArgumentException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex);
+                    return;
+                }
+
+                throw new Exception("Error occured when trying to get the first element: " + ex);
+            }
+            finally
+            {
+                Console.WriteLine("\nPlease enter a new string:");
+                input = Console.ReadLine();
+                GetFirstChar(input);
+            }
         }
     }
 }
